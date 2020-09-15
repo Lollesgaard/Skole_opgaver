c3a3fc94aa31b9864da4a298728267d36f59630d
Lollesgaard <jonaslolle@gmail.com>
2020-09-15 08:31:23 +0200

    færdig med opgave 1.1

---
+++ opgave_loops.py
@@ -0,0 +1,32 @@
+# for i in range(1,101):
+#     print('Hello World')
+
+# for i in range(1,11):
+#     print(i)
+
+#     if i == 5:
+#         print('Det her er tallet')
+
+
+# for i in range(1,26):
+#     if i %2 == 0:
+#         print(i)
+
+
+# var1 = []
+
+# for i in range(1,4):
+    
+#     var1.append(input('Skriv noget!'))
+
+# print(var1)
+
+# for i in range(100):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print('FizzBuzz')
+#     elif i % 3 == 0:
+#         print('Fizz')
+#     elif i % 5 == 0:
+#         print('Buzz')
+
+
